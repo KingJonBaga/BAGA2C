e3adfa3337177f3972eb698d8cc28b9ef9e3742a
KingJonBaga <bagakingjon@gmail.com>
2025-08-04 17:28:21 +0800

    activity

--- src/baga/baga4.java
+++ src/baga/baga4.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package baga1;
+package baga;
 
 import java.util.Scanner;
 
@@ -13,33 +13,31 @@ import java.util.Scanner;
  */
 public class baga4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter Employee Name: ");
-        String employeeName = scanner.nextLine();
-        
-        System.out.print("Enter hourly rate: ");
-        double hourlyRate = scanner.nextDouble();
-        
-        System.out.print("Enter total hours worked this week: ");
-        double hoursWorked = scanner.nextDouble();
-        
-        double grossWage = hourlyRate * hoursWorked;
-        double contribution = grossWage * 0.10;
-        double netWage = grossWage - contribution;
-        
-        System.out.println("\nOutput: ");
-        System.out.println("----Wage Summary----");
-        
-        System.out.println("Employee: " + employeeName);
-        
-        System.out.println("Hourly Rate :₱%.2f%n");
-        System.out.printf("Hours Worked: %.0f%n", hoursWorked);
-        System.out.printf("Contribution (10%%):₱%.2f%n", contribution);
-        
-        System.out.println("-------------------------------");
-        System.out.printf("Net Weekly Wage:₱%.2f%n", netWage);
-        
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter Employee Name: ");
+            String employeeName = scanner.nextLine();
+            
+            System.out.print("Enter hourly rate: ");
+            double hourlyRate = scanner.nextDouble();
+            
+            System.out.print("Enter total hours worked this week: ");
+            double hoursWorked = scanner.nextDouble();
+            
+            double grossWage = hourlyRate * hoursWorked;
+            double contribution = grossWage * 0.10;
+            double netWage = grossWage - contribution;
+            
+            System.out.println("\nOutput: ");
+            System.out.println("----Wage Summary----");
+            
+            System.out.println("Employee: " + employeeName);
+            
+            System.out.println("Hourly Rate :₱%.2f%n");
+            System.out.printf("Hours Worked: %.0f%n", hoursWorked);
+            System.out.printf("Contribution (10%%):₱%.2f%n", contribution);
+            
+            System.out.println("-------------------------------");
+            System.out.printf("Net Weekly Wage:₱%.2f%n", netWage);
+        }
     }
 }
